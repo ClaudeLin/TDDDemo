91a0009a063286f99e0c9dea01d42f02c6f0d811
ClaudeLin <linc27595@gmail.com>
2016-12-10 20:37:32 +0800

    add new kata 100 doors

---
+++ TDDDemo/TDDDemo/OneHundredDoors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDDemo
+{
+    public class OneHundredDoors
+    {
+        private bool doorStatus = false;
+
+        public void Visit(int visitCount=1)
+        {
+            for (var i = 1; i <= visitCount; i++)
+            {
+                if (100%i == 0)
+                {
+                    doorStatus = !doorStatus;
+                }
+            }
+        }
+
+        public bool GetLastDoorStatus()
+        {
+            return doorStatus;
+        }
+    }
+}
---
+++ TDDDemo/TDDDemoTest/OneHundredDoorsTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDDemo;
+
+namespace TDDDemoTest
+{
+    [TestClass]
+    public class OneHundredDoorsTest
+    {
+        private OneHundredDoors doors;
+        private bool actual;
+        [TestMethod]
+        public void TestVisitOneTime()
+        {
+            doors =new OneHundredDoors();
+            doors.Visit();
+
+            actual = doors.GetLastDoorStatus();
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void TestVisitTwoTimes()
+        {
+            doors=new OneHundredDoors();
+            doors.Visit(2);
+            
+
+            actual = doors.GetLastDoorStatus();
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void TestVisitThreeTimes()
+        {
+            doors = new OneHundredDoors();
+            doors.Visit(3);
+           
+
+            actual = doors.GetLastDoorStatus();
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void TestVisitFourTimes()
+        {
+            doors = new OneHundredDoors();
+            doors.Visit(4);
+
+
+            actual = doors.GetLastDoorStatus();
+
+            Assert.IsTrue(actual);
+        }
+
+
+        [TestMethod]
+        public void TestVisit100Times()
+        {
+            doors = new OneHundredDoors();
+            doors.Visit(100);
+
+
+            actual = doors.GetLastDoorStatus();
+
+            Assert.IsTrue(actual);
+        }
+
+    }
+}
